176ec8585b45ad3b26a0834c1df48b77b9ffa635
zhouzhiqiang <mumu@appadhoc.com>
2016-12-07 14:57:58 +0800

    update tmux.conf,add zshrc and mkLinks

---
+++ mkLinks.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+link_file() {
+	SOURCE_FILE=/Users/zhouzhiqiang/dotfiles/$1
+	DESTINATION_FILE=/Users/zhouzhiqiang/$2
+	if [ -h $DESTINATION_FILE ] ; then
+		echo -n "remove symbolic link: "
+		rm -v $DESTINATION_FILE
+	elif [ -e $DESTINATION_FILE ]; then
+		echo -n "backup $DESTINATION_FILE: "
+		mv -v $DESTINATION_FILE $DESTINATION_FILE.bak
+	fi
+
+	echo -n "link file: "
+	ln -sv $SOURCE_FILE $DESTINATION_FILE
+}
+
+link_file tmux.conf .tmux.conf 
+link_file zshrc .zshrc
